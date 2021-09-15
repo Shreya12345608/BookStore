e3c05279b556385d188fdf81552987911023c62f
Shreya <malviyashreya26@gmail.com>
2021-09-15 19:45:35 +0530

    [Shreya]Add: :bookmark_tabs: :sunny: Created DashBoard in Book Store  :notebook_with_decorative_cover: :woman:

--- BookStore/src/app/component/dashboard/dashboard.component.ts
+++ BookStore/src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  profileDialogShown:boolean = false;
+  userSignedOff:boolean = true;
 
-  constructor() { }
+
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
   }
-
+  profileDialogShownToggle(){
+    this.profileDialogShown = ! this.profileDialogShown;
+  }
+  SignOut(){
+    localStorage.clear();
+    this.route.navigate(['login']);
+  }
+  Login(){
+    this.route.navigate(['login']);
+  }
+  showCart(){
+    this.route.navigate(['dashboard/cart']);
+  }
 }
